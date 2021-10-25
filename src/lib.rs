c38d00bb2d9c6da2046a03a7e8622d1714fd4e0c
Johannes Draaijer <johannes@widefind.se>
2021-10-25 10:51:23 +0200

    Add Pin struct that does common operations

--- src/lib.rs
+++ src/lib.rs
@@ -4,73 +4,89 @@ use std::io::prelude::*;
 
 pub use embedded_hal::digital::v2::OutputPin;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct OutPin {
-    num: u32,
-}
-
 type Error = io::Error;
 
-impl OutPin {
+struct Pin;
+
+impl Pin {
     /// Calculates the pins number for usage with the export and unexport files found on the linux
     /// system
-    fn convert_to_absolute(port: u8, index: u8) -> u32 {
+    pub(crate) fn convert_to_absolute(port: u8, index: u8) -> u32 {
         let port = port as u32;
         let index = index as u32;
         let outnum = (port - 1) * 32 + index;
         outnum
     }
 
+    pub(crate) fn force_reset_abs(num: u32) {
+        if let Ok(mut export) = File::create("/sys/class/gpio/unexport") {
+            let _e = export.write(num.to_string().as_bytes());
+        }
+    }
+
+    pub(crate) fn force_reset(port: u8, index: u8) {
+        let num = Pin::convert_to_absolute(port, index);
+        Self::force_reset_abs(num);
+    }
+
+    pub(crate) fn init(port: u8, index: u8, direction: &str) -> Result<u32, Error> {
+        let num = Self::convert_to_absolute(port, index);
+        let mut export = File::create("/sys/class/gpio/export")?;
+        export.write(num.to_string().as_bytes())?;
+
+        let mut f_direction = File::create(format!("/sys/class/gpio/gpio{}/direction", num))?;
+        f_direction.write(direction.as_bytes())?;
+        Ok(num)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct OutPin {
+    num: u32,
+}
+
+impl OutPin {
     /// Resets the pins by unexporting the pins from userspace through its file interface, to reset its state, then configures a new
     /// pin. This should make sure that the pin is usable.
     ///
     /// Note: It does not take into account if other
     /// applications are using the pins or anything like that.
     pub fn force_new(port: u8, index: u8) -> Result<Self, Error> {
-        let num = Self::convert_to_absolute(port, index);
-        if let Ok(mut export) = File::create("/sys/class/gpio/unexport") {
-            let _e = export.write(num.to_string().as_bytes());
-        }
-
+        Pin::force_reset(port, index);
         Self::new(port, index)
     }
 
     /// Tries to export and configure a new output pin, this can error out due to the pin already
     /// configured, usually with a device or resource busy
     pub fn new(port: u8, index: u8) -> Result<Self, Error> {
-        let num = Self::convert_to_absolute(port, index);
-        let mut export = File::create("/sys/class/gpio/export")?;
-        export.write(num.to_string().as_bytes())?;
-
-        let mut direction = File::create(format!("/sys/class/gpio/gpio{}/direction", num))?;
-        direction.write("out".as_bytes())?;
-
+        let num = Pin::init(port, index, "out")?;
         Ok(OutPin { num })
+    }
+
+    fn write_output(&mut self, value: &str) -> Result<(), Error> {
+        let mut direction = File::create(format!("/sys/class/gpio/gpio{}/value", self.num))?;
+        direction.write(value.as_bytes())?;
+        Ok(())
     }
 }
 
 impl OutputPin for OutPin {
-    type Error = io::Error;
+    type Error = Error;
 
     fn set_low(&mut self) -> Result<(), Error> {
-        let mut direction = File::create(format!("/sys/class/gpio/gpio{}/value", self.num))?;
-        direction.write("0".as_bytes())?;
-        Ok(())
+        self.write_output("0")
     }
 
     fn set_high(&mut self) -> Result<(), Error> {
-        let mut direction = File::create(format!("/sys/class/gpio/gpio{}/value", self.num))?;
-        direction.write("1".as_bytes())?;
-        Ok(())
+        self.write_output("1")
     }
 }
+
 // Implement drop so that we can remove the pin from memory and setup once it is not to be used
 // anymore
 impl Drop for OutPin {
     fn drop(&mut self) {
-        if let Ok(mut unexport) = File::create("/sys/class/gpio/unexport") {
-            let _e = unexport.write(self.num.to_string().as_bytes());
-        }
+        Pin::force_reset_abs(self.num);
     }
 }
 
